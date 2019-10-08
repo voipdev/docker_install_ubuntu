8cb91a21f73309fb22b382682ea8e18417102991
DAR <voipdev@users.noreply.github.com>
2019-10-08 10:19:39 -0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
+# Installation script for docker engine, containerd and docker-compose
+# OS requirements: Ubuntu Bionic/Xenial
+
 # remove previous installs
 sudo apt-get remove docker docker-engine docker.io
 
-sudo apt-get update
-sudo apt-get install \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  software-properties-common
-
-curl  -fsSL https://download.docker.com/linux/ubuntu/gpg |\
-  sudo apt-key add -
-sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-
-
 sudo apt-get -y update
-sudo apt-get -y install docker-ce
+
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# update package index
+sudo apt-get -y update
+
+# install docker engine & containerd
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+# install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# make binary executable
+sudo chmod +x /usr/local/bin/docker-compose
 
 
