c910188f0307482a56aee9c7a2ffca41256befb8
DAR <voipdev@users.noreply.github.com>
2020-06-15 10:14:28 -0700

    update docker-compose version to 1.26.0

--- install_docker.sh
+++ install_docker.sh
@@ -30,7 +30,7 @@ sudo apt-get -y update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 # install docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 # make binary executable
 sudo chmod +x /usr/local/bin/docker-compose
